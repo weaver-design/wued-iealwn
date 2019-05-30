036df652a53f3f787b37833fdeb5bc2f31c94de0
Dengju Deng <wj871287@gmail.com>
2019-05-30 16:51:04 +0800

    Create lastes.js

---
+++ lastes.js
@@ -0,0 +1,35 @@
+/** 
+ * ------------------------------------------------------------------ 
+ *
+ *                 当前链接为 CDN 分流缓存，具有时效性
+ *                  引用代码请使用永久链接以保持最新
+ * 
+ *                   -------- 永久链接 --------
+ * 
+ *         https://support.dmeng.net/ie-alert-warning/latest.js
+ * 
+ *                   -------- 代码说明 --------
+ * 
+ *           https://support.dmeng.net/ie-alert-warning.html
+ * 
+ * ------------------------------------------------------------------ 
+ */ 
+ 
+// work in IE 6/7/8/9/10
+
+/*@cc_on
+
+    var _iealwn = _iealwn || [];
+    _iealwn.dir = 'https://netcdn.dmeng.net/ie-alert-warning';
+
+    (function() {
+
+        var s = document.getElementById('_iealwn_js');
+
+        var tag = document.createElement('script');
+        tag.src = _iealwn.dir + '/latest.js.cached.js';
+
+        s.parentNode.insertBefore(tag, s);
+    })();
+    
+@*/
